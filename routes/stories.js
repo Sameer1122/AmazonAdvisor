a83bde7e7e60c22ffec4e88bc8c4e8e0e94cc2e3
Sameer Ahmed <sameer.ssj786@gmail.com>
2021-10-18 01:45:54 +0500

    make it better

---
+++ routes/stories.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { check } from "express-validator";
+import { postStories } from "../controllers/Stories.js";
+import { getAllStories } from "../controllers/Stories.js";
+import { deletePlace } from "../controllers/Stories.js";
+import { updatePlace } from "../controllers/Stories.js";
+import { fileUpload } from "../middleware/file-upload.js";
+const router = express.Router();
+router.get("/", getAllStories);
+router.patch(
+  "/:uid",
+  fileUpload.single("img"),
+  [check("title").not().isEmpty(), check("description").not().isEmpty()],
+  updatePlace
+);
+router.post(
+  "/",
+  fileUpload.single("img"),
+  [
+    check("title").not().isEmpty(),
+    check("sideBar").not().isEmpty(),
+    check("views").not().isEmpty(),
+    check("description").not().isEmpty(),
+  ],
+  postStories
+);
+router.delete("/:pid", deletePlace);
+export default router;
